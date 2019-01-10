59470b6f009d26b2406ba2008fce932db6f03bfb
Umang Mathur <umangmathur10051992@gmail.com>
2019-01-09 16:35:13 -0800

    Added actions, outlets and a method to calculate tip

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func onTap(_ sender: AnyObject) {
+    @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15, 0.18, 0.2]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
